2351fbd07923a8394e2025ffdc57652c681aa0ee
HaydenGaia <hayden.richards@gaiaresources.com.au>
2022-10-05 15:24:26 +0800

    Fix spacing in settings.py

--- backend/kaartdijin_boodja/settings.py
+++ backend/kaartdijin_boodja/settings.py
@@ -60,7 +60,6 @@ TEMPLATES = [
 ]
 WSGI_APPLICATION = "kaartdijin_boodja.wsgi.application"
 
-
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 DATABASES = {
@@ -69,7 +68,6 @@ DATABASES = {
         "NAME": BASE_DIR / "db.sqlite3",
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
