e3051846a82715f81b76c88246bd6006fb85b26d
HaydenGaia <hayden.richards@gaiaresources.com.au>
2022-10-05 16:03:25 +0800

    Fix django project settings

--- backend/kaartdijin_boodja/settings.py
+++ backend/kaartdijin_boodja/settings.py
@@ -13,6 +13,10 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 # Standard
 import pathlib
 
+# Third-Party
+import decouple
+import dj_database_url
+
 
 # Build paths inside the project like this: BASE_DIR / "subdir".
 BASE_DIR = pathlib.Path(__file__).resolve().parent.parent
@@ -20,9 +24,12 @@ BASE_DIR = pathlib.Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-SECRET_KEY = "django-insecure"  # SECURITY WARNING: keep the secret key used in production secret!
-DEBUG = True  # SECURITY WARNING: don't run with debug turned on in production!
-ALLOWED_HOSTS = ["*"]  # SECURITY WARNING: don't allow any host in production!
+# SECURITY WARNING: keep the secret key used in production secret!
+# SECURITY WARNING: don't run with debug turned on in production!
+# SECURITY WARNING: don't allow any host in production!
+SECRET_KEY = decouple.config("SECRET_KEY")
+DEBUG = decouple.config("DEBUG", default=False, cast=bool)
+ALLOWED_HOSTS = decouple.config("ALLOWED_HOSTS", default=["*"] if DEBUG else [])
 
 # Application definition
 INSTALLED_APPS = [
@@ -65,10 +72,7 @@ WSGI_APPLICATION = "kaartdijin_boodja.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    "default": decouple.config("DATABASE_URL", cast=dj_database_url.parse),
 }
 
 # Password validation
