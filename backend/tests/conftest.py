9bd680575c1e78d8277b58dd3f454351cb75835f
HaydenGaia <hayden.richards@gaiaresources.com.au>
2022-10-20 13:56:29 +0800

    Update unit tests, 100% coverage, settings overrides, sqlite in-memory database, model fixture loading.

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -7,12 +7,14 @@ It may be also used for extending doctest's context:
 
 
 # Third-Party
+from django.core import management
 import pytest
 from pytest_django import fixtures
+from pytest_django import plugin
 
 
 @pytest.fixture(autouse=True)
-def _debug(settings: fixtures.SettingsWrapper) -> None:
+def debug(settings: fixtures.SettingsWrapper) -> None:  # noqa: PT004
     """Sets proper DEBUG and TEMPLATE debug mode for coverage.
 
     Args:
@@ -24,3 +26,22 @@ def _debug(settings: fixtures.SettingsWrapper) -> None:
     # Set templates to debug mode
     for template in settings.TEMPLATES:
         template["OPTIONS"]["debug"] = True
+
+
+@pytest.fixture()
+def db_fixtures(  # noqa: PT004
+    django_db_setup: None,
+    django_db_blocker: plugin._DatabaseBlocker,
+    settings: fixtures.SettingsWrapper
+) -> None:
+    """Loads model test fixtures into the database.
+
+    Args:
+        django_db_setup (None): Pytest Django dependency.
+        django_db_blocker (plugin._DatabaseBlocker): Pytest Django dependency.
+        settings (fixtures.SettingsWrapper): Pytest Django settings fixture.
+    """
+    # Unblock Database
+    with django_db_blocker.unblock():
+        # Load Fixtures
+        management.call_command("loaddata", *settings.FIXTURES)
